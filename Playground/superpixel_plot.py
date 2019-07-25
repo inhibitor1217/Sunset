c6e408c976a9152de9ddc1c2d8d65e8295fc4401
inhibitor1217 <cytops1217@gmail.com>
2019-07-25 14:41:41 +0900

    implemented weighted SVD

--- Playground/superpixel_plot.py
+++ Playground/superpixel_plot.py
@@ -87,13 +87,27 @@ def slic_stats(img, slic_label):
 
     return num_labels, segment_count, segment_avg, segment_stdev[:, 0]
 
-def pca(X):
+def weighted_quantile(data, quantile, weights=None):
+    if weights is None:
+        return np.quantile(data, quantile)
+    else:
+        ind = np.argsort(data)
+        data_sorted = data[ind]
+        weights_sorted = weights[ind]
+        return np.interp(quantile, np.cumsum(weights_sorted) / np.sum(weights_sorted), data_sorted)
+
+def pca(X, weights=None):
     # Linear Fit (PCA) using SVD
-    X_bar = X.mean(axis=0)
-    U, S, Vt = np.linalg.svd(X - X_bar)
+    X_bar   = np.average(X, axis=0, weights=weights)
+    if weights is None:
+        X_prime = X - X_bar
+    else:
+        X_prime = (X - X_bar) * np.sqrt(weights)[..., np.newaxis]
+    
+    U, S, Vt = np.linalg.svd(X_prime)
     FPC = Vt[0]
-    X_V = np.matmul(X - X_bar, Vt.transpose())
-    TSS = np.sum(np.square(X - X_bar))
+    X_V = np.matmul(X_prime, Vt.transpose())
+    TSS = np.sum(np.square(X_prime))
     RSS = np.sum(np.square(X_V[:, 1:]))
     R_squared = 1 - RSS/TSS
 
@@ -101,23 +115,23 @@ def pca(X):
     print('PCA: Direction = ' + str(FPC))
     print('PCA: R^2       = ' + str(R_squared))
 
-    FPC_distribution = X_V[:, 0]
+    FPC_distribution = np.matmul(X - X_bar, Vt.transpose())[:, 0]
+    quartiles = weighted_quantile(FPC_distribution, [.03, .10, .20, .30, .40, .50, .60, .70, .80, .90, .97], weights)
+    FPC_line = X_bar[..., np.newaxis].repeat(11, axis=1) + np.matmul(FPC[..., np.newaxis], quartiles[..., np.newaxis].transpose())
 
-    return X_bar, FPC, FPC_distribution, R_squared
+    return X_bar, FPC, R_squared, FPC_distribution, FPC_line
 
 def draw_image(img, window_title='Display'):
     cv2.imshow(window_title, img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def slic_plot_stats(slic_num_segments, slic_segment_avg, slic_segment_stdev):
-    # Create plot
-    fig1 = plt.figure()
-    ax1 = fig1.add_subplot(111, projection='3d')
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
+def plot_slic_scatter(slic_num_segments, slic_segment_avg, file_name, line_fit=None):
+    # Create Figure
+    fig = plt.figure(figsize=(20, 20))
+    ax  = fig.add_subplot(111, projection='3d')
 
-    # Process Scatter Plot Colors
+    # Generate Scatter Plot Colors
     slic_segment_avg_rgb = np.zeros((1, slic_num_segments, 3))
     slic_segment_avg_rgb[0, :, 0]  = slic_segment_avg[:, 0]  * (255/100)
     slic_segment_avg_rgb[0, :, 1:] = slic_segment_avg[:, 1:] + 128
@@ -125,90 +139,124 @@ def slic_plot_stats(slic_num_segments, slic_segment_avg, slic_segment_stdev):
     plot_colors = (slic_segment_avg_rgb[0, :, :] / 255).tolist()
 
     # Scatter Plot
-    ax1.scatter(
+    ax.scatter(
         slic_segment_avg[:, 1].tolist(),
         slic_segment_avg[:, 2].tolist(),
         slic_segment_avg[:, 0].tolist(),
         s=1,
         c=plot_colors,
-        marker='o'
+        marker='o',
+        alpha=.15
     )
-    ax1.set_xlabel('A*')
-    ax1.set_ylabel('B*')
-    ax1.set_zlabel('L*')
-    ax1.set_xlim((-32, 32))
-    ax1.set_ylim((-32, 32))
-    ax1.set_zlim((0, 100))
 
-    ax2.scatter(
+    # Line Plot
+    if line_fit is not None:
+        ax.plot(
+            line_fit[1, :],
+            line_fit[2, :],
+            line_fit[0, :],
+            's-r',
+        )
+
+    ax.set_xlabel('A*')
+    ax.set_ylabel('B*')
+    ax.set_zlabel('L*')
+    ax.set_xlim((-32, 32))
+    ax.set_ylim((-32, 32))
+    ax.set_zlim((0, 100))
+
+    # Save Figure
+    fig.savefig('plots/' + file_name + '-SPXScatter.png')
+    plt.close(fig)
+
+def plot_slic_stdev(slic_num_segments, slic_segment_avg, slic_segment_stdev, file_name):
+    # Create Figure
+    fig = plt.figure(figsize=(20, 20))
+    ax  = fig.add_subplot(111)
+
+    # Generate Scatter Plot Colors
+    slic_segment_avg_rgb = np.zeros((1, slic_num_segments, 3))
+    slic_segment_avg_rgb[0, :, 0]  = slic_segment_avg[:, 0]  * (255/100)
+    slic_segment_avg_rgb[0, :, 1:] = slic_segment_avg[:, 1:] + 128
+    slic_segment_avg_rgb = cv2.cvtColor(np.uint8(slic_segment_avg_rgb), cv2.COLOR_LAB2RGB)
+    plot_colors = (slic_segment_avg_rgb[0, :, :] / 255).tolist()
+
+    # Scatter Plot
+    ax.scatter(
         slic_segment_avg[:, 0].tolist(),
         slic_segment_stdev.tolist(),
         s=1,
         c=plot_colors,
         marker='o'
     )
-    ax2.set_xlabel('L*')
-    ax2.set_ylabel('STDEV(L*)')
 
-    return fig1, fig2
+    ax.set_xlabel('L*')
+    ax.set_ylabel('STDEV(L*)')
+
+    # Save Figure
+    fig.savefig('plots/' + file_name + '-SPXSTDL.png')
+    plt.close(fig)
+
+def plot_slic_distribution(distribution, file_name, weights=None):
+    # Create Figure
+    fig = plt.figure(figsize=(20, 20))
+    ax = fig.add_subplot(111)
+    
+    # Histogram
+    ax.hist(distribution, bins=1000, weights=weights)
+    
+    # Save Figure
+    fig.savefig('plots/' + file_name + '-GLOBALL.png')
+    plt.close(fig)
 
 file_names = [
     'jj-1-mask',
-    'jj-2-mask',
-    'sc-1-mask',
-    'sc-2-mask',
-    'sc-3-mask',
-    'sc-4-mask',
-    'sf-1-mask',
-    'sf-2-mask',
-    'sf-3-mask',
-    'sf-4-mask',
-    'sf-5-mask',
-    'sf-6-mask',
-    'sf-7-mask',
-    'sf-8-mask',
-    'sf-9-mask',
-    'sf-10-mask',
-    'sf-11-mask',
-    'sf-12-mask',
-    'sf-13-mask',
-    'sf-14-mask',
-    'ys-4-mask',
+    # 'jj-2-mask',
+    # 'sc-1-mask',
+    # 'sc-2-mask',
+    # 'sc-3-mask',
+    # 'sc-4-mask',
+    # 'sf-1-mask',
+    # 'sf-2-mask',
+    # 'sf-3-mask',
+    # 'sf-4-mask',
+    # 'sf-5-mask',
+    # 'sf-6-mask',
+    # 'sf-7-mask',
+    # 'sf-8-mask',
+    # 'sf-9-mask',
+    # 'sf-10-mask',
+    # 'sf-11-mask',
+    # 'sf-12-mask',
+    # 'sf-13-mask',
+    # 'sf-14-mask',
+    # 'ys-4-mask',
 ]
 
 if __name__ == "__main__":
 
-    # f = open('linreg.csv', 'w')
-    # columns = ['FILE_NAME', 'GLOBAL_EQ CENTER L*', 'GLOBAL_EQ CENTER A*', 'GLOBAL_EQ CENTER B*', 'GLOBAL_EQ FPC L*', 'GLOBAL_EQ FPC A*', 'GLOBAL_EQ FPC B*', 'GLOBAL_EQ R^2']
+    # f = open('linreg-PIX.csv', 'w')
+    # columns = ['FILE_NAME', 'GLOBAL_PIX CENTER L*', 'GLOBAL_PIX CENTER A*', 'GLOBAL_PIX CENTER B*', 'GLOBAL_PIX FPC L*', 'GLOBAL_PIX FPC A*', 'GLOBAL_PIX FPC B*', 'GLOBAL_PIX R^2']
     # f.write(','.join(columns) + '\n')   
 
     for file_name in file_names:
         img = read_image('images/mask/' + file_name + '.png')
         img_lab = preprocess(img)
-        # num_seg, label, contour = slic(img_lab)
-        label = decode_label(cv2.imread('images/slic/' + file_name + '.png'))
 
+        # num_seg, label, contour = slic(img_lab)
         # cv2.imwrite('images/slic/' + file_name + '.png', encode_label(label))
+        
+        label = decode_label(cv2.imread('images/slic/' + file_name + '.png'))
         
         # draw_image(slic_contour(img, contour))
         num_labels, count, avg, stdev = slic_stats(img_lab, label)
-        center, fpc, distribution, r2 = pca(avg)
+        center, fpc, r2, distribution, line = pca(avg, weights=count)
 
         # data = [file_name, center[0], center[1], center[2], fpc[0], fpc[1], fpc[2], r2]
         # f.write(','.join(list(map(str, data))) + '\n')
 
-        plt.style.use(['dark_background'])
-        # fig1, fig2 = slic_plot_stats(num_labels, avg, stdev)
-        # fig1.savefig('plots/' + file_name + '-plot1.png')
-        # fig2.savefig('plots/' + file_name + '-plot2.png')
-        # plt.close(fig1)
-        # plt.close(fig2)
-
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.hist(distribution, bins=1000, weights=count)
-        fig.savefig('plots/' + file_name + '-plot3.png')
-        plt.close(fig)
+        # plt.style.use(['dark_background'])
+        # plot_slic_scatter(num_labels, avg, file_name, line_fit=line)
     
     # f.close()
 
